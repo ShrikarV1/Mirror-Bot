3b106d3bbbecc3137669c0873925f782ed6eef02
ShrikarV1 <107179697+ShrikarV1@users.noreply.github.com>
2022-06-21 13:53:45 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Owner", "https://t.me/AnshumanPM")
-    buttons.buildbutton("Authorised Group", "https://t.me/+mRNtIt7g9jJhYmNl")
+    buttons.buildbutton("Owner", "https://t.me/Shrikar2006")
+    buttons.buildbutton("Authorised Group", "https://t.me/+6WqrscrxF2AzMmRl")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
